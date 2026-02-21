8761c34c37a31d41821e61e3a81d8df7c503843e
tirthbhavsar-code <tirth.bhavsar@binarypenguin.co>
2026-02-21 12:20:37 +0530

    added

--- create_schema.py
+++ create_schema.py
@@ -1,11 +1,12 @@
 import psycopg2
-from psycopg2 import sql
 from config import DB_CONFIG
+
 
 def create_database():
     try:
         conn = psycopg2.connect(
             host=DB_CONFIG["host"],
+            database="postgres",  # connect to default DB
             user=DB_CONFIG["user"],
             password=DB_CONFIG["password"],
             port=DB_CONFIG["port"]
@@ -20,7 +21,7 @@ def create_database():
             cursor.execute("CREATE DATABASE healthcare_db")
             print("Database created successfully!")
         else:
-            print("ℹ Database already exists.")
+            print("Database already exists.")
 
         cursor.close()
         conn.close()
@@ -31,10 +32,10 @@ def create_database():
 
 def create_table():
     try:
-        conn = psycopg2.connect(**DB_CONFIG)
+        conn = psycopg2.connect(**DB_CONFIG)  # must include database
         cursor = conn.cursor()
 
-        create_table_query = """
+        cursor.execute("""
         CREATE TABLE IF NOT EXISTS patients (
             Patient_ID VARCHAR(50) PRIMARY KEY,
             Full_Name VARCHAR(150) NOT NULL,
@@ -45,11 +46,10 @@ def create_table():
             Last_Visit_Date DATE,
             Ingestion_Timestamp TIMESTAMP
         );
-        """
+        """)
 
-        cursor.execute(create_table_query)
         conn.commit()
-        print("Table 'patients' created successfully!")
+        print("Table created successfully!")
 
         cursor.close()
         conn.close()
